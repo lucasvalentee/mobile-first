df58d2b6823c2196ce7606a72a6b69c1dd7fdb8b
Lucas Valente <lucas.valente@asksuite.com>
2022-03-30 20:08:31 -0300

    refactor: Removed not used lines.

--- src/pages/SpaceMissions/styles.ts
+++ src/pages/SpaceMissions/styles.ts
@@ -6,13 +6,9 @@ export const Content = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  /* justify-content: center; */
-
-  /* place-content: center; */
 
   width: 100%;
   height: 100%;
-  /* max-width: 700px; */
 
   img {
     margin: 16px 0px;
